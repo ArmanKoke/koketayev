50f5b3206caa873802f6604b2c5cbc5f1d7fa9c3
ako <ako@paybox.money>
2020-05-28 18:59:35 +0600

    created Title.vue, deleted title from index.html

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
+    <Title/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Title from './components/Title.vue';
+  import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      Title,
+      HelloWorld
+    }
   }
-}
 </script>
 
 <style>
---
+++ src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+    <title v-text="title"></title>
+</template>
+
+<script>
+    export default {
+        name: 'VueTitle',
+        props: ['title'],
+        watch: {
+            title: {
+                immediate: true,
+                handler() {
+                    document.title = process.env.VUE_APP_TITLE;
+                }
+            }
+        },
+    }
+</script>
